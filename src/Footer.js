22606620b1a5eb33c2e62039d06995ec65c3c040
Fazil <fazilala14@gmail.com>
2023-11-21 03:23:14 +0530

    commit

--- src/Footer.js
+++ src/Footer.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="text-white bg-dark  text-center w-100 p-1 " >
-      <p className='justify-content-center'>&copy; 2023 Fazil Store. All rights reserved.</p>
+    <footer className="text-white bg-dark text-center fixed-bottom">
+      <p>&copy; 2023 Fazil Store. All rights reserved.</p>
     </footer>
   );
 };
 
 export default Footer;
+
